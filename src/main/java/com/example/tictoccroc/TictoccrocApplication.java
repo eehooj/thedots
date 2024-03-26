0873e01261225ee08d869a3ec4d89b46287e9511
joohee_jeong <jooh21020@gmail.com>
2024-03-27 00:41:25 +0900

    first commit

---
+++ src/main/java/com/example/tictoccroc/TictoccrocApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictoccroc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictoccrocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictoccrocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tictoccroc/TictoccrocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictoccroc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictoccrocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
